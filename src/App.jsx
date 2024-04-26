b8f118be928d2bd3b9c3a18fd1d4f05defa71e70
2G2-99 <sagos0919@gmail.com>
2024-04-26 21:12:32 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <main>
+        {/* <!-- Sign-up form start --> */}
+        Stay updated! Join 60,000+ product managers receiving monthly updates
+        on: Product discovery and building what matters Measuring to ensure
+        updates are a success And much more! Email address email@company.com
+        Subscribe to monthly newsletter
+        {/* <!-- Sign-up form end --> */}
+        {/* <!-- Success message start --> */}
+        Thanks for subscribing! A confirmation email has been sent to
+        ash@loremcompany.com. Please open it and click the button inside to
+        confirm your subscription. Dismiss message
+        {/* <!-- Success message end --> */}
+      </main>
+      <footer>
+        <div className="attribution">
+          <p>
+            Challenge by{' '}
+            <a
+              href="https://www.frontendmentor.io?ref=challenge"
+              target="_blank"
+            >
+              Frontend Mentor
+            </a>
+            . Coded by <a href="#">Santiago Gomez</a>.
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default App;

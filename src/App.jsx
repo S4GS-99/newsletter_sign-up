7d0e6e316042a680c58719b0a7c5a7b7346ec5b0
2G2-99 <sagos0919@gmail.com>
2024-05-03 08:27:25 +0100

    Sign-up component finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,21 @@
 import { useState } from 'react';
 import './App.css';
-import SignUp from './components/Sign-up';
+import SignUp from './components/SignUp';
 import Success from './components/Success';
 
 function App() {
   const [submitted, setSubmitted] = useState(false);
   const handleSubmit = () => {
-    setSubmitted(true);
+    setTimeout(() => {
+      setSubmitted(true);
+    }, 500);
   };
 
   return (
     <div className="content-grid">
-      <main className="feature">
-        {submitted === true ? <SignUp /> : <Success />}
+      <main className="full-width">
+        {submitted === false ? <SignUp onSubmit={handleSubmit} /> : <Success />}
       </main>
-      <footer>
-        <div className="attribution">
-          <p>
-            Challenge by{' '}
-            <a
-              href="https://www.frontendmentor.io?ref=challenge"
-              target="_blank"
-            >
-              Frontend Mentor
-            </a>
-            . Coded by <a href="#">Santiago Gomez</a>.
-          </p>
-        </div>
-      </footer>
     </div>
   );
 }

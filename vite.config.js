7cf4fffaaf2d198d5fc1478ae344de380be879d0
2G2-99 <sagos0919@gmail.com>
2024-05-06 19:45:31 +0100

    Both components are finished

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [eslint(), react()],
   test: {
     globals: true,
     environment: 'jsdom',
